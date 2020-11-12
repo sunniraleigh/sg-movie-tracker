e86d9594ceb8160a9056e5f5b90e0e74a6a25418
sunniraleigh <sraleigh@smith.edu>
2020-11-12 20:04:19 +0000

    secure secret_key

--- server.py
+++ server.py
@@ -5,10 +5,11 @@ from flask import (Flask, render_template, request, flash,
 from model import connect_to_db
 import crud
 from jinja2 import StrictUndefined
+import os
 
 app = Flask(__name__)
 
-app.secret_key = "vaergdiaegviiqGEWOAERGIDLHA"
+app.secret_key = os.environ['APP_SECRET_KEY']
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
