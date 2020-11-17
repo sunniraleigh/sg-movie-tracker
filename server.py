8412334e0679fa6aac39cdd97623fe0ebd840ff3
sunniraleigh <sraleigh@smith.edu>
2020-11-17 03:16:26 +0000

    add logout route to server

--- server.py
+++ server.py
@@ -78,6 +78,14 @@ def login_user():
         flash('Username or email incorrect. Please try again.')
         return redirect('/login')
 
+@app.route('logout', methods=['POST'])
+def logout_user():
+    """Logs a user out."""
+
+    del session['current_user']
+    flash('Logged out.')
+    return redirect('/')
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
